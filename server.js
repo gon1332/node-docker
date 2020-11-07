38959ee32f4f97933dec0ed62c9cc0cd87a3672c
Ioannis Konstantelias <ikonstadel@gmail.com>
2020-11-07 13:25:57 +0200

    Use Express as server
    Also host and port are command line inputs.

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
-const ronin = require( 'ronin-server' )
-const mocks = require( 'ronin-mocks' )
+'use strict'
 
-const server = ronin.server()
+const express   = require('express')
 
-server.use( '/', mocks.server( server.Router(), false, true ) )
-server.start()
+// Parse argumets
+const args = require('minimist')(process.argv.slice(2))
+if (!('host' in args) || !('port' in args)) {
+    console.log('host or port were not specified')
+    process.exit(1)
+}
+
+const host = args['host']
+const port = args['port']
+
+// App
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hi everyone')
+})
+
+const server = app.listen(port, host, () => {
+    console.log(`Running on http://${host}:${port}`)
+    console.log('Brought by', process.env.USER)
+})
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        console.log('\nServer closed')
+    })
+})
