5f8ad8b5fc857e233f00124c2477e194a1ad8ab0
Ioannis Konstantelias <ikonstadel@gmail.com>
2020-11-09 21:15:37 +0200

    Use EJS for templating
    Also added a simple webpage

--- server.js
+++ server.js
@@ -15,8 +15,25 @@ const port = args['port'];
 // App
 const app = express();
 
+// Set view engine to EJS
+app.set('view engine', 'ejs');
+
+// index page
 app.get('/', (req, res) => {
-  res.send('Hey you');
+  res.render('pages/index');
+});
+
+// about page
+app.get('/about', (req, res) => {
+  var quotes = [
+    'Everything in moderation - including moderation.',
+    'Common sense is not a common thing.',
+    'To go wrong in one\'s own way is better than to go right in someone' +
+    ' else\'s.',
+  ];
+  res.render('pages/about', {
+    quotes: quotes,
+  });
 });
 
 const server = app.listen(port, host, () => {
